37aacab2e0318d3a8ba661460ba53efb4bc96619
Nick <65930023+Nick67644@users.noreply.github.com>
2022-05-08 05:02:18 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Starting glitch
+
+Questions I have about the platform...
+
+[start-here](https://start-here.glitch.me/)
---
+++ script.js
@@ -0,0 +1,11 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+var csshead = "font-size: 40px; font-weight: bold; color: red"
+
+consolefun();
+
+function consolefun() {
+    console.warn("%cIf someone told you to copy/paste something here they may be stealing your information", csshead);
+}
